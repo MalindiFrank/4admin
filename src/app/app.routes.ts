48908f164391fead00ddefe697717df1a1416c35
thembalakhe <m.themba1738@gmail.com>
2025-01-12 15:41:16 +0200

    set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { authGuard } from './auth/auth.guard';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: '/login', component: AdminComponent, canActivate: [authGuard]},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
