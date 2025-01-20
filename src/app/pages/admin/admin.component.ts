1610f54fd44f9a5ed6fd0c71561a5657c68c5697
thembalakhe <thmalinjhb024@student.wethinkcode.co.za>
2025-01-20 15:34:07 +0200

    updated dashboard ui

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api/api.service';
-import { AddProductComponent } from '../../components/add-product/add-product.component';
 import { DashboardComponent } from "../../components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-admin',
-  imports: [AddProductComponent, DashboardComponent],
+  imports: [ DashboardComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
